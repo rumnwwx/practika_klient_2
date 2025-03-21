546ac474bec77a1d92b4156e3c328394ef0dee5a
rumnwwx <elchaninapolina@gmail.com>
2025-03-21 11:30:58 +0700

    create card

--- vue/js/main.js
+++ vue/js/main.js
@@ -1,17 +1,29 @@
 let app = new Vue({
     el: '#app',
-    data() {
-        return {
-            columns: {
-                todo: [],
-                inProgress: [],
-                completed: [],
-            },
-            columnTitles: {
-                todo: 'начато',
-                inProgress: 'к работе',
-                completed: 'завершено',
-            },
-        };
+    data: {
+        columns: {
+            todo: [],
+            inProgress: [],
+            completed: [],
+        },
+        columnTitles: {
+            todo: 'Начато',
+            inProgress: 'К работе',
+            completed: 'Завершено',
+        },
+    },
+    methods: {
+        addNote(column) {
+            const newNote = {
+                id: Date.now(),
+                title: 'Новая заметка',
+                items: [
+                    { text: 'Пункт 1', completed: false },
+                    { text: 'Пункт 2', completed: false },
+                    { text: 'Пункт 3', completed: false },
+                ],
+            };
+            this.columns[column].push(newNote);
+        },
     }
 });
